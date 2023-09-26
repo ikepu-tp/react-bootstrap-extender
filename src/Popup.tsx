f774292c496e11fb2f25e26badb2db1bc12561ca
ikepu-tp <postmaster@licsss.jp>
2023-09-26 11:46:14 +0900

    fix

--- src/Popup.tsx
+++ src/Popup.tsx
@@ -1,19 +1,15 @@
-import React from 'react';
-import { Modal } from 'react-bootstrap';
+import { Modal, ModalProps } from 'react-bootstrap';
 
-export type PopupProps = React.PropsWithChildren & {
-	show: boolean;
-	size?: 'sm' | 'lg' | 'xl';
-	onHide: () => void;
-	header: string | JSX.Element | any;
+export type PopupProps = ModalProps & {
+	header?: string | JSX.Element | any;
 	footer?: string | JSX.Element | any;
 };
-export default function Popup(props: PopupProps): JSX.Element {
+export default function Popup({ header, footer, children, ...props }: PopupProps): JSX.Element {
 	return (
-		<Modal show={props.show} size={props.size} onHide={props.onHide} animation scrollable centered>
-			<Modal.Header closeButton>{props.header}</Modal.Header>
-			<Modal.Body>{props.children}</Modal.Body>
-			{props.footer && <Modal.Footer>{props.footer}</Modal.Footer>}
+		<Modal animation scrollable centered {...props}>
+			{header && <Modal.Header closeButton>{header}</Modal.Header>}
+			<Modal.Body>{children}</Modal.Body>
+			{footer && <Modal.Footer>{footer}</Modal.Footer>}
 		</Modal>
 	);
 }
