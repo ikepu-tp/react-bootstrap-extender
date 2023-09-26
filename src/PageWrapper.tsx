c3bc81f992f3c96bf19e3058fa0a9e9c95cbe2ee
ikepu-tp <postmaster@licsss.jp>
2023-09-26 23:12:06 +0900

    update breadCrumbOnClick

--- src/PageWrapper.tsx
+++ src/PageWrapper.tsx
@@ -1,4 +1,4 @@
-import React, { MouseEventHandler } from 'react';
+import React, { MouseEvent } from 'react';
 import { Breadcrumb } from 'react-bootstrap';
 
 export type BreadCrumbType = {
@@ -8,7 +8,7 @@ export type BreadCrumbType = {
 export type PageWrapperProps = React.PropsWithChildren & {
 	title: string;
 	breadCrumb?: BreadCrumbType[];
-	breadCrumbOnClick?: MouseEventHandler<HTMLAnchorElement>;
+	breadCrumbOnClick?: (e: MouseEvent<HTMLAnchorElement>, item: BreadCrumbType) => void;
 };
 export default function PageWrapper(props: PageWrapperProps): JSX.Element {
 	return (
@@ -19,7 +19,7 @@ export default function PageWrapper(props: PageWrapperProps): JSX.Element {
 					<Breadcrumb.Item href="/">HOME</Breadcrumb.Item>
 					{props.breadCrumb.map(
 						(item: BreadCrumbType): JSX.Element => (
-							<BreadcrumbItem key={item.text} item={item} onClick={props.breadCrumbOnClick} />
+							<BreadcrumbItem key={item.text} item={item} navigate={props.breadCrumbOnClick} />
 						)
 					)}
 					<Breadcrumb.Item active>{props.title}</Breadcrumb.Item>
@@ -30,9 +30,17 @@ export default function PageWrapper(props: PageWrapperProps): JSX.Element {
 	);
 }
 
-function BreadcrumbItem(props: { item: BreadCrumbType; onClick?: MouseEventHandler<HTMLAnchorElement> }): JSX.Element {
+function BreadcrumbItem(props: {
+	item: BreadCrumbType;
+	navigate?: (e: MouseEvent<HTMLAnchorElement>, item: BreadCrumbType) => void;
+}): JSX.Element {
+	function onClick(e: MouseEvent<HTMLAnchorElement>): void {
+		if (!props.navigate) return;
+		e.preventDefault();
+		props.navigate(e, props.item);
+	}
 	return (
-		<Breadcrumb.Item href={props.item.link} onClick={props.onClick}>
+		<Breadcrumb.Item href={props.item.link} onClick={onClick}>
 			{props.item.text}
 		</Breadcrumb.Item>
 	);
