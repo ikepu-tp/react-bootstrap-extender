6025afcaca973a58e5bc5c01aee773414d1bea24
ikepu-tp <postmaster@licsss.jp>
2023-09-26 11:32:09 +0900

    add index

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { default as Anchor } from './Anchor';
+export { default as ListView } from './ListView';
+export { default as PageWrapper } from './PageWrapper';
+export { default as Paginate } from './Paginate';
+export { default as Popup } from './Popup';
