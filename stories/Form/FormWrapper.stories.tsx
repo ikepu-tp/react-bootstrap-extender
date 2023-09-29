c88217c8bd6dc74dff76e175f6935462e950c47c
ikepu-tp <postmaster@licsss.jp>
2023-09-30 05:55:18 +0900

    add SB

---
+++ stories/Form/FormWrapper.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { FormWrapper, ResponseResource } from './../../src/Form';
+import React, { useState } from 'react';
+import { Button } from 'react-bootstrap';
+
+const meta = {
+	title: 'Forms/FormWrapper',
+	component: FormWrapperComponent,
+	tags: ['autodocs'],
+	argTypes: {},
+} satisfies Meta<typeof FormWrapperComponent>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Normal: Story = {
+	args: {
+		successMessage: '処理に成功しました',
+	},
+};
+
+function FormWrapperComponent(props: { successMessage: string }): JSX.Element {
+	const [ButtonDisabled, setButtonDisabled] = useState<boolean>(false);
+	async function onSubmit(): Promise<ResponseResource> {
+		return {};
+	}
+	return (
+		<FormWrapper onSubmit={onSubmit} setButtonDisabled={setButtonDisabled} successMessage={props.successMessage}>
+			<Button variant="primary" type="submit" disabled={ButtonDisabled}>
+				送信
+			</Button>
+		</FormWrapper>
+	);
+}
