ddd3f6f099c4697d9b059c3ddb8a87c2e10bcd19
ikepu-tp <postmaster@licsss.jp>
2023-09-30 05:29:40 +0900

    update pagination

--- src/Paginate.tsx
+++ src/Paginate.tsx
@@ -7,7 +7,7 @@ export type PaginateProps = {
 };
 export default function Paginate(props: PaginateProps): JSX.Element {
 	function toPrev(): void {
-		props.setPage(Math.max(props.meta.currentPage, 1));
+		props.setPage(Math.max(props.meta.currentPage - 1, 1));
 	}
 	function toNext(): void {
 		props.setPage(Math.min(props.meta.lastPage, props.meta.currentPage + 1));
@@ -22,9 +22,21 @@ export default function Paginate(props: PaginateProps): JSX.Element {
 		<Pagination>
 			<Pagination.First onClick={toFirst} disabled={props.meta.currentPage === 1} />
 			<Pagination.Prev onClick={toPrev} disabled={props.meta.currentPage === 1} />
-			<Pagination.Item disabled>{props.meta.currentPage}</Pagination.Item>
+			{props.meta.currentPage !== 1 && (
+				<>
+					{props.meta.currentPage - 1 !== 1 && <Pagination.Ellipsis />}
+					<Pagination.Item onClick={toPrev}>{props.meta.currentPage - 1}</Pagination.Item>
+				</>
+			)}
+			<Pagination.Item active>{props.meta.currentPage}</Pagination.Item>
+			{props.meta.currentPage !== props.meta.lastPage && (
+				<>
+					<Pagination.Item onClick={toNext}>{props.meta.currentPage + 1}</Pagination.Item>
+					<Pagination.Ellipsis />
+				</>
+			)}
 			<Pagination.Next onClick={toNext} disabled={props.meta.lastPage === props.meta.currentPage} />
-			<Pagination.Last onCanPlay={toLast} disabled={props.meta.currentPage === props.meta.lastPage} />
+			<Pagination.Last onClick={toLast} disabled={props.meta.currentPage === props.meta.lastPage} />
 		</Pagination>
 	);
 }
