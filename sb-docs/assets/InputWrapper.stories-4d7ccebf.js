64aa2edaec1b0df1f5e237dae75a42d4666b9983
ikepu-tp <postmaster@licsss.jp>
2023-10-02 07:31:14 +0900

    v0.8.0

--- sb-docs/assets/InputWrapper.stories-4d7ccebf.js
+++
@@ -1,9 +0,0 @@
-import{j as p}from"./jsx-runtime-ffb262ed.js";import{I as i,F as s}from"./ListView-803e4df6.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./Paginate-6788492f.js";import"./useEventCallback-f3900921.js";import"./Anchor-0b0fe176.js";import"./index-8d47fad6.js";import"./Popup-8a83400c.js";import"./index-d3ea75b5.js";import"./inheritsLoose-c82a83d4.js";import"./extends-98964cd2.js";import"./Anchor-f8aa5777.js";import"./PageWrapper-ed421ee1.js";const I={title:"Forms/InputWrapper",component:i,tags:["autodocs"],argTypes:{}},r={args:{label:"項目1",required:!0,children:p.jsx(s.Control,{type:"text",placeholder:"テキスト..."}),description:"description"}};var o,t,e;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    label: '項目1',
-    required: true,
-    children: <Form.Control type="text" placeholder="テキスト..." />,
-    description: 'description'
-  }
-}`,...(e=(t=r.parameters)==null?void 0:t.docs)==null?void 0:e.source}}};const b=["Normal"];export{r as Normal,b as __namedExportsOrder,I as default};
-//# sourceMappingURL=InputWrapper.stories-4d7ccebf.js.map
