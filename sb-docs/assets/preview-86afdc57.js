e33f7a3d861cf7659199f0a19b46c12ded666f88
ikepu-tp <postmaster@licsss.jp>
2023-10-02 22:57:42 +0900

    build on v0.9.0

---
+++ sb-docs/assets/preview-86afdc57.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./iframe-59d71534.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-3PUGWF3O-a0e9a443.js").then(e=>e.D),["./DocsRenderer-3PUGWF3O-a0e9a443.js","./iframe-59d71534.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./_getPrototype-725791a0.js","./index-d3ea75b5.js","./index-d37d4223.js","./extends-98964cd2.js","./inheritsLoose-c82a83d4.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-86afdc57.js.map
