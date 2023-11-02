e6e7a2841a6bf1b7dffe964abea8317285bfeb3b
ikepu-tp <postmaster@licsss.jp>
2023-11-02 22:58:08 +0900

    PageWrapperのBreadCrumbリンク Fixes #32

--- src/PageWrapper.tsx
+++ src/PageWrapper.tsx
@@ -1,4 +1,4 @@
-import React, { MouseEvent } from 'react';
+import React, { MouseEvent, MouseEventHandler } from 'react';
 import { Breadcrumb } from 'react-bootstrap';
 
 export type BreadCrumbType = {
@@ -9,6 +9,8 @@ export type PageWrapperProps = React.PropsWithChildren & {
 	title: string;
 	breadCrumb?: BreadCrumbType[];
 	breadCrumbOnClick?: (e: MouseEvent<HTMLAnchorElement>, item: BreadCrumbType) => void;
+	homeText?: React.ReactNode;
+	onClick?: MouseEventHandler<HTMLAnchorElement>;
 };
 export default function PageWrapper(props: PageWrapperProps): JSX.Element {
 	return (
@@ -16,10 +18,12 @@ export default function PageWrapper(props: PageWrapperProps): JSX.Element {
 			<h3>{props.title}</h3>
 			{props.breadCrumb && (
 				<Breadcrumb className="my-2">
-					<Breadcrumb.Item href="/">HOME</Breadcrumb.Item>
+					<Breadcrumb.Item href="/" onClick={props.onClick}>
+						{props.homeText || 'HOME'}
+					</Breadcrumb.Item>
 					{props.breadCrumb.map(
 						(item: BreadCrumbType): JSX.Element => (
-							<BreadcrumbItem key={item.text} item={item} navigate={props.breadCrumbOnClick} />
+							<BreadcrumbItem key={item.text} item={item} navigate={props.breadCrumbOnClick} onClick={props.onClick} />
 						)
 					)}
 					<Breadcrumb.Item active>{props.title}</Breadcrumb.Item>
@@ -33,10 +37,17 @@ export default function PageWrapper(props: PageWrapperProps): JSX.Element {
 function BreadcrumbItem(props: {
 	item: BreadCrumbType;
 	navigate?: (e: MouseEvent<HTMLAnchorElement>, item: BreadCrumbType) => void;
+	onClick?: MouseEventHandler<HTMLAnchorElement>;
 }): JSX.Element {
 	function onClick(e: MouseEvent<HTMLAnchorElement>): void {
-		if (!props.navigate) return;
-		props.navigate(e, props.item);
+		if (props.onClick) {
+			props.onClick(e);
+			return;
+		}
+		if (props.navigate) {
+			props.navigate(e, props.item);
+			return;
+		}
 	}
 	return (
 		<Breadcrumb.Item href={props.item.link} onClick={onClick}>
