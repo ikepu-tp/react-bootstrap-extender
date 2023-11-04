3a1cb9a4e9d1bc34efcdfd44eeb6a54de1bc51bd
ikepu-tp <postmaster@licsss.jp>
2023-11-04 10:00:04 +0900

    fix

--- src/PageWrapper.tsx
+++ src/PageWrapper.tsx
@@ -11,6 +11,7 @@ export type PageWrapperProps = React.PropsWithChildren & {
 	breadCrumbOnClick?: (e: MouseEvent<HTMLAnchorElement>, item: BreadCrumbType) => void;
 	homeText?: React.ReactNode;
 	onClick?: MouseEventHandler<HTMLAnchorElement>;
+	changeLink?: (link: string) => void;
 	changeTitle?: boolean;
 	siteTitle?: string;
 	titleSeparator?: string;
@@ -22,17 +23,28 @@ export default function PageWrapper(props: PageWrapperProps): JSX.Element {
 		if (props.siteTitle) title.push(props.siteTitle);
 		document.title = title.join(` ${props.titleSeparator || '-'} `);
 	}, [props.title]);
+
+	function homeOnClick(e: MouseEvent<HTMLAnchorElement>): void {
+		if (props.changeLink) return props.changeLink('/');
+		if (props.onClick) return props.onClick(e);
+	}
 	return (
 		<div>
 			<h3>{props.title}</h3>
 			{props.breadCrumb && (
 				<Breadcrumb className="my-2">
-					<Breadcrumb.Item href="/" onClick={props.onClick}>
+					<Breadcrumb.Item href="/" onClick={homeOnClick}>
 						{props.homeText || 'HOME'}
 					</Breadcrumb.Item>
 					{props.breadCrumb.map(
 						(item: BreadCrumbType): JSX.Element => (
-							<BreadcrumbItem key={item.text} item={item} navigate={props.breadCrumbOnClick} onClick={props.onClick} />
+							<BreadcrumbItem
+								key={item.text}
+								item={item}
+								navigate={props.breadCrumbOnClick}
+								onClick={props.onClick}
+								changeLink={props.changeLink}
+							/>
 						)
 					)}
 					<Breadcrumb.Item active>{props.title}</Breadcrumb.Item>
@@ -47,16 +59,12 @@ function BreadcrumbItem(props: {
 	item: BreadCrumbType;
 	navigate?: (e: MouseEvent<HTMLAnchorElement>, item: BreadCrumbType) => void;
 	onClick?: MouseEventHandler<HTMLAnchorElement>;
+	changeLink?: (link: string) => void;
 }): JSX.Element {
 	function onClick(e: MouseEvent<HTMLAnchorElement>): void {
-		if (props.navigate) {
-			props.navigate(e, props.item);
-			return;
-		}
-		if (props.onClick) {
-			props.onClick(e);
-			return;
-		}
+		if (props.navigate) return props.navigate(e, props.item);
+		if (props.changeLink) return props.changeLink(props.item.link);
+		if (props.onClick) return props.onClick(e);
 	}
 	return (
 		<Breadcrumb.Item href={props.item.link} onClick={onClick}>
