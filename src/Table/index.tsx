51718dfadc69f358c2f82a3f7e5075a38466290f
ikepu-tp <postmaster@licsss.jp>
2024-07-21 09:11:44 +0900

    add Table

---
+++ src/Table/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { TableProps as BaseTableProps, Table as BaseTable } from 'react-bootstrap';
+import './style.css';
+
+export type TableProps = BaseTableProps & {
+	fixedTitle?: boolean;
+	wrapperHeight?:string
+};
+export default function Table({ fixedTitle,wrapperHeight, ...props }: TableProps): React.ReactNode {
+	if(!fixedTitle) return <BaseTable {...props} />;
+
+	const WrapperStyle: React.CSSProperties = {
+		height: wrapperHeight,
+	};
+
+	return (
+		<>
+			<div className={'all-on-screen'} style={WrapperStyle}>
+				<div className={'fixed-table__wrapper'}>
+					<BaseTable {...props} className={`${props.className || ''} fixed-table`} />
+				</div>
+			</div>
+		</>
+	);
+}
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ export { default as Paginate } from './Paginate';
 export { default as Popup } from './Popup';
 export { default as Status, StatusWrapper } from './Status';
 export { StatusContext, useStatus } from './StatusContext';
+export {default as Table} from './Table'
 
 export type { AlertBoxProps } from './AlertBox';
 export type { AnchorProps } from './Anchor';
@@ -14,6 +15,7 @@ export type { BreadCrumbType, PageWrapperProps } from './PageWrapper';
 export type { PaginateProps } from './Paginate';
 export type { PopupProps } from './Popup';
 export type { StatusResource, StatusContextType } from './StatusContext';
+export type {TableProps} from "./Table"
 
 export function createKey(): string {
 	return Math.random().toString(32).substring(2);
